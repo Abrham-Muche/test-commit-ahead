17f8da08743002442aa5dd504b757b807f4a4389
Abrham-Muche <abrham.m@code-care.pro>
2024-02-13 19:57:00 +0300

    add entry point

---
+++ app.js
@@ -0,0 +1,8 @@
+// This is the entry point of the program
+function main() {
+    // Write your code here
+    console.log("Hello, world!");
+  }
+  
+  // Call the main function to start the program
+  main();
